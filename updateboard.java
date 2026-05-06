65e9267861f7aef3f28167a30ffe21b631dea395
haileycross-sudo <hailey.cross@mcstn.org>
2026-05-06 19:02:04 +0000

    WHT IS HAPENEIDNGE

--- updateboard.java
+++ updateboard.java
@@ -8,33 +8,15 @@ public class updateboard {
 
   public static void draw3DBoard(String[][][] board) {
 
-    for (int outerRow = 0; outerRow < 3; outerRow++) {
-      for (int innerRow = 0; innerRow < 3; innerRow++) {
-        for (int outerCol = 0; outerCol < 3; outerCol++) {
-          int blockIndex = outerRow * 3 + outerCol;
-          String[][] block = board[blockIndex];
-          if (block == null || block.length != 3) {
-            throw new IllegalArgumentException("Each block must be a 3x3 array.");
-          }
-
-          for (int innerCol = 0; innerCol < 3; innerCol++) {
-            String value = block[innerRow][innerCol];
-            System.out.print(value == null ? " " : value);
-            if (innerCol < 2) {
-              System.out.print(" |");
-            }
-          }
-
-          if (outerCol < 2) {
-            System.out.print(" || ");
-          }
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 3; j++) {
+        for (int k = 0; k < 3; k++) {
+          System.out.print(board[i][j][k] + " ");
         }
         System.out.println();
       }
-
-      if (outerRow < 2) {
-        System.out.println("========++========++========");
-      }
+      System.out.println();
     }
+
   }
 }
